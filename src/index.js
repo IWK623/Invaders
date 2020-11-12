61df040e8752298fee921c7acdad7abd08b96578
IWK623 <kei6023.mcpe@gmail.com>
2020-11-12 23:43:54 +0900

    a

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import BootScene from './scenes/BootScene';
 import LoaderScene from './scenes/LoaderScene';
 import GameScene from './scenes/GameScene';
 
+console.log("Hello.....!!!!!1");
+
 var config = {
   type: Phaser.AUTO,
   parent: 'phaser-example',
